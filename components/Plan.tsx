1e77b9c6c244616fc704d5841feb50306794f953
MladoniSzabi <szabimlado@gmail.com>
2023-04-24 14:32:25 +0100

    made it possible to select plans and added hover and active effects to them.

--- components/Plan.tsx
+++ components/Plan.tsx
@@ -3,12 +3,17 @@ import * as React from 'react'
 import style from './Plan.module.css'
 
 export default function Plan({ icon, type, price, children = null }) {
-    return <div className={style['plan-container']}>
-        <img src={icon} alt={'icon for ' + type + ' plan'}></img>
-        <div>
-            <h4>{type}</h4>
-            <p>{price}</p>
-            {children}
-        </div>
+    return <div className={style['container']}>
+        <input type="radio" id={type} name="plan" />
+        <label htmlFor={type}>
+            <div className={style['plan-container']}>
+                <img src={icon} alt={'icon for ' + type + ' plan'}></img>
+                <div>
+                    <h4>{type}</h4>
+                    <p>{price}</p>
+                    {children}
+                </div>
+            </div>
+        </label>
     </div>
 }
